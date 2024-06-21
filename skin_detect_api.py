e872de1bf6a4576a93e87ae8e34e39f034baca84
programmeraditya36@gmail.com <programmeraditya36@gmail.com>
2024-06-21 08:20:27 +0530

    Initial commit

---
+++ skin_detect_api.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from tensorflow.keras.models import load_model # type: ignore
+import numpy as np
+model_path = 'model_checkpoint.weights.keras'
+model = load_model(model_path)
+app = FastAPI()
+class SkinDetectRequest(BaseModel):
+    data: list
+@app.post("/predict")
+def predict_skin_detect(request: SkinDetectRequest):
+    data = np.array(request.data)
+    predictions = model.predict(data)
+    class_names = ['Actinic keratoses', 'Basal cell carcinoma', 'Benign keratosis-like lesions', 'Dermatofibroma', 'Melanoma', 'Melanocytic nevi', 'Vascular lesions']
+    predicted_class_index = np.argmax(predictions[0])
+    predicted_class_name = class_names[predicted_class_index]
+    result = f'Predicted class: {predicted_class_name} with probability {predictions[0][predicted_class_index]:.4f}'
+    return {"result": result}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
